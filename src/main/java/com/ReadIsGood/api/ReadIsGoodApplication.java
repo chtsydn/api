8dbab834ac1fd1e97cf5033a2914b02292b59d02
chtsydn <chtsydn@hotmail.com>
2022-12-25 01:59:18 +0300

    Initial commit

---
+++ src/main/java/com/ReadIsGood/api/ReadIsGoodApplication.java
@@ -0,0 +1,13 @@
+package com.ReadIsGood.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadIsGoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReadIsGoodApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ReadIsGood/api/ReadIsGoodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ReadIsGood.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReadIsGoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
